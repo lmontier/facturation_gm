b6576bc717060e89faa3d5f5e78700b2e03dd3ec
Laurent Montier <laurent.montier@gmail.com>
2025-02-12 16:46:31 +0100

    feat: Add precommit

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv("test.csv", sep='\t')
+df = pd.read_csv("test.csv", sep="\t")
 
 
-group_colums = ["resa_dossier","resa_ocup_nom","resa_ocup_prenom","lot_ref"]
+group_colums = ["resa_dossier", "resa_ocup_nom", "resa_ocup_prenom", "lot_ref"]
 
 
 groups = {}
 for name, group in df.groupby(group_colums):
-    data = group.loc[:,["prest_titre_fra", "resa_prest_qte"]].set_index("prest_titre_fra").resa_prest_qte.to_dict()
+    data = (
+        group.loc[:, ["prest_titre_fra", "resa_prest_qte"]]
+        .set_index("prest_titre_fra")
+        .resa_prest_qte.to_dict()
+    )
     groups[name] = data
-# data = df.groupby(group_colums).apply(
-#     lambda g: {g["prest_titre_fra"]: g["resa_prest_qte"]}
-# )
-   
-# print(data.to_csv("done.csv"))
+
 print(groups)
 print("coucou")
 
