54c8e5ab5e78035b2b2c499ca139620568dec6ad
laurent montier <laurent.montier@gmail.com>
2025-02-13 16:18:18 +0100

    Use excel (#1)
    * feat: Add xlrd to read excel files
    
    * Update script
    
    * feat: Update st_demo
    
    * lint

--- script.py
+++ script.py
@@ -1,13 +1,26 @@
 import pandas as pd
+import json
 
-df = pd.read_csv("test.csv", sep="\t")
+df = pd.read_excel("data/extraction_2025.xls")
 
 
-group_colums = ["resa_dossier", "resa_ocup_nom", "resa_ocup_prenom", "lot_ref"]
+def format_name(_df: pd.DataFrame) -> pd.Series:
+    return _df.resa_ocup_civi + " " + _df.resa_ocup_prenom + " " + _df.resa_ocup_nom
+
+
+GROUP_COLUMS = [
+    "resa_dossier",
+    "resa_ocup_nom",
+    "resa_ocup_prenom",
+    "resa_ocup_civi",
+    "lot_ref",
+    "resa_deb",
+    "resa_fin",
+]
 
 
 groups = {}
-for name, group in df.groupby(group_colums):
+for name, group in df.groupby(GROUP_COLUMS):
     data = (
         group.loc[:, ["prest_titre_fra", "resa_prest_qte"]]
         .set_index("prest_titre_fra")
@@ -15,9 +28,21 @@ for name, group in df.groupby(group_colums):
     )
     groups[name] = data
 
-print(groups)
-print("coucou")
+
+# %%
+exploded_df = (
+    pd.DataFrame(groups)
+    .T.reset_index()
+    .rename(columns={f"level_{i}": GROUP_COLUMS[i] for i in range(len(GROUP_COLUMS))})
+    .assign(Nom=lambda _df: _df.pipe(format_name))
+)
 
 
-pd.DataFrame(groups).T.fillna(0).to_csv("done.csv")
-# pd.DataFrame(groups).to_csv("done.csv")
+# %% Map data
+
+with open("column_mapping.json", "r") as file:
+    column_mapping = json.load(file)
+final_df = exploded_df.rename(columns=column_mapping).reindex(
+    columns=column_mapping.values()
+)
+final_df.to_csv("data/done.csv")
--- st_demo.py
+++ st_demo.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import streamlit as st
+import json
 
-GROUP_COLUMS = ["resa_dossier", "resa_ocup_nom", "resa_ocup_prenom", "lot_ref"]
+GROUP_COLUMS = [
+    "resa_dossier",
+    "resa_ocup_nom",
+    "resa_ocup_prenom",
+    "resa_ocup_civi",
+    "lot_ref",
+    "resa_deb",
+    "resa_fin",
+]
+
+
+with open("column_mapping.json", "r") as file:
+    COLUMN_MAPPING = json.load(file)
+
+
+def format_name(_df: pd.DataFrame) -> pd.Series:
+    return _df.resa_ocup_civi + " " + _df.resa_ocup_prenom + " " + _df.resa_ocup_nom
 
 
 @st.cache_data
 def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode("utf-8")
 
 
 def main():
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, sep="\t")
+        df = pd.read_excel(uploaded_file)
 
         with st.expander("Voir le fichier:", expanded=False):
             st.dataframe(df)
@@ -27,13 +43,26 @@ def main():
             )
             groups[name] = data
 
-        result_df = pd.DataFrame(groups).T.fillna(0)
+        # %%
+        exploded_df = (
+            pd.DataFrame(groups)
+            .T.reset_index()
+            .rename(
+                columns={
+                    f"level_{i}": GROUP_COLUMS[i] for i in range(len(GROUP_COLUMS))
+                }
+            )
+            .assign(Nom=lambda _df: _df.pipe(format_name))
+        )
 
-        st.dataframe(result_df)
+        final_df = exploded_df.rename(columns=COLUMN_MAPPING).reindex(
+            columns=COLUMN_MAPPING.values()
+        )
+        st.dataframe(final_df)
 
         st.download_button(
             label="Download data as CSV",
-            data=convert_df(result_df),
+            data=convert_df(final_df),
             file_name="extract_conversion.csv",
             mime="text/csv",
         )
